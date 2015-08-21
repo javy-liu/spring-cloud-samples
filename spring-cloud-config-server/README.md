d4514005ff41268b8d6754fe1cd8c95400596ed6
oyach <lzy7750015@gmail.com>
2015-08-21 19:30:21 +0800

    添加config server例子

---
+++ spring-cloud-config-server/README.md
@@ -0,0 +1,1 @@
+# spring cloud config server 例子
--- spring-cloud-config-server/src/test/java/org/oyach/cloud/config/SpringCloudConfigServerApplicationTests.java
+++ spring-cloud-config-server/src/test/java/org/oyach/cloud/config/SpringCloudConfigServerApplicationTests.java
@@ -2,15 +2,44 @@ package org.oyach.cloud.config;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringCloudConfigServerApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port=0")
 public class SpringCloudConfigServerApplicationTests {
 
+	@Value("${local.server.port}")
+	private int port = 0;
+
 	@Test
-	public void contextLoads() {
+	public void configurationAvailable() {
+		ResponseEntity<Map> entity = new TestRestTemplate().getForEntity(
+				"http://localhost:" + port + "/configserver/default", Map.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+	}
+
+	@Test
+	public void envPostAvailable() {
+		MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+
+		ResponseEntity<Map> entity = new TestRestTemplate().postForEntity(
+				"http://localhost:" + port + "/admin/env", form, Map.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
 	}
 
 }
